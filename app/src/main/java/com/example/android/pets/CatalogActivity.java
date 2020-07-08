ef08d3caca9b0b5dc5146803aa0a4b7241132bc5
Kushan Tharaka <tharakakushan@gmail.com>
2020-07-08 18:27:46 +0530

    Uri method created in catelog activity to insert data as test by MenueItem in there

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -163,14 +163,23 @@ public class CatalogActivity extends AppCompatActivity {
         // Create a ContentValues object where column names are the keys,
         // and Toto's pet attributes are the values.
         ContentValues values = new ContentValues();
-        values.put(PetDataEntry.COLUMN_PET_NAME, "Toto");
+        values.put(PetDataEntry.COLUMN_PET_NAME, "TotoTest");
         values.put(PetDataEntry.COLUMN_PET_BREED, "Terrier");
         values.put(PetDataEntry.COLUMN_PET_GENDER, PetDataEntry.GENDER_MALE);
-        values.put(PetDataEntry.COLUMN_PET_WEIGHT, 7);
+        values.put(PetDataEntry.COLUMN_PET_WEIGHT, 9);
 
 
-        long newRowId = db.insert(PetDataEntry.TABLE_NAME, null, values);
-        Log.i("CatalogActivity", "New Row Id" + newRowId);
+       //Below method is so vulnerable so we have to deduct it by commenting
+        //have declare new method for the inserting data to the database
+        /* long newRowId = db.insert(PetDataEntry.TABLE_NAME, null, values);
+        Log.i("CatalogActivity", "New Row Id" + newRowId); */
+
+                //   Insert a new row for Toto into the provider using the ContentResolver.
+                // Use the {@link PetEntry#CONTENT_URI} to indicate that we want to insert
+                // into the pets database table.
+                // Receive the new content URI that will allow us to access Toto's data in the future.
+        Uri newUri = getContentResolver().insert(PetDataEntry.CONTENT_URI,values);
+
     }
 
     @Override
