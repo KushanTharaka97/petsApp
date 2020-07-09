132f2aa53461b834dd798fceea34330f2b0979fc
Kushan Tharaka <tharakakushan@gmail.com>
2020-07-09 23:28:16 +0530

    bindView method was created/implemented.

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -2,9 +2,13 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.pets.data.PetContract.PetDataEntry;
 
 public class PetCursorAdapter extends CursorAdapter {
 
@@ -12,13 +16,41 @@ public class PetCursorAdapter extends CursorAdapter {
         super(context,c,0);
     }
 
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
     }
 
+    /**
+     * This method binds the pet data (in the current row pointed to by cursor) to the given
+     * list item layout. For example, the name for the current pet can be set on the name TextView
+     * in the list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        TextView name = view.findViewById(R.id.name);
+        TextView breed = view.findViewById(R.id.summary);
 
+        // Extract properties from cursor
+        String body = cursor.getString(cursor.getColumnIndexOrThrow(PetDataEntry.COLUMN_PET_NAME));
+        int priority = cursor.getInt(cursor.getColumnIndexOrThrow(PetDataEntry.COLUMN_PET_BREED));
+
+        // Populate fields with extracted properties
+        name.setText(body);
+        breed.setText(String.valueOf(priority));
     }
 }
