f9bf1e05db0ee1bf94aedf6559299c0ab762a816
Kushan Tharaka <tharakakushan@gmail.com>
2020-07-10 22:24:56 +0530

    Cursor Wrong initialization error done

--- app/src/main/java/com/example/android/pets/EditorActivity.java
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -18,12 +18,9 @@ package com.example.android.pets;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -41,7 +38,6 @@ import androidx.loader.app.LoaderManager;
 import androidx.loader.content.CursorLoader;
 import androidx.loader.content.Loader;
 
-import com.example.android.pets.data.PetContract;
 import com.example.android.pets.data.PetContract.PetDataEntry;
 import com.example.android.pets.data.PetDbHelper;
 
@@ -217,16 +213,18 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
 
     @NonNull
     @Override
-    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         //projection define
         String[] projection = {
                 PetDataEntry._ID,
                 PetDataEntry.COLUMN_PET_NAME,
+                PetDataEntry.COLUMN_PET_BREED,
                 PetDataEntry.COLUMN_PET_GENDER,
                 PetDataEntry.COLUMN_PET_WEIGHT
         };
 
-        return new CursorLoader(this,
+        return new CursorLoader(
+                this,
                 mCurrentPetUri,
                 projection,
                 null,
@@ -236,13 +234,53 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
     }
 
     @Override
-    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
-        //updating the new cursor
-        mCursorAdapter.swapCursor(data);
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+
+        if (cursor == null|| cursor.getCount() < 1) {
+            return;
+        }
+
+        if (cursor.moveToNext()) {
+            int nameColumnIndex = cursor.getColumnIndex(PetDataEntry.COLUMN_PET_NAME);
+            int breedColumnIndex = cursor.getColumnIndex(PetDataEntry.COLUMN_PET_BREED);
+            int genderColumnIndex = cursor.getColumnIndex(PetDataEntry.COLUMN_PET_GENDER);
+            int weightColumnIndex = cursor.getColumnIndex(PetDataEntry.COLUMN_PET_WEIGHT);
+
+            // Extract out the value from the Cursor for the given column index
+            String name = cursor.getString(nameColumnIndex);
+            String breed = cursor.getString(breedColumnIndex);
+            int gender = cursor.getInt(genderColumnIndex);
+            int weight = cursor.getInt(weightColumnIndex);
+
+            mNameEditText.setText(name);
+            mBreedEditText.setText(breed);
+            mWeightEditText.setText(weight);
+
+            // Gender is a dropdown spinner, so map the constant value from the database
+            // into one of the dropdown options (0 is Unknown, 1 is Male, 2 is Female).
+            // Then call setSelection() so that option is displayed on screen as the current selection.
+            switch (gender){
+                case PetDataEntry.GENDER_MALE:
+                    mGenderSpinner.setSelection(1);
+                    break;
+
+                    case PetDataEntry.GENDER_FEMALE:
+                        mGenderSpinner.setSelection(2);
+                        break;
+
+                    default:
+                    mGenderSpinner.setSelection(0);
+                    break;
+            }
+        }
     }
 
     @Override
     public void onLoaderReset(@NonNull Loader<Cursor> loader) {
-        mCursorAdapter.swapCursor(null);
+        // If the loader is invalidated, clear out all the data from the input fields.
+        mNameEditText.setText("");
+        mBreedEditText.setText("");
+        mWeightEditText.setText("");
+        mGenderSpinner.setSelection(0); // Select "Unknown" gender
     }
 }
